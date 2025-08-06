59dd4085c0bd31e7b139c635b93b2a8d909cd4cc
Leonardo-Scotti <scottileonardo1@gmail.com>
2025-08-05 22:42:30 -0300

    Adicionando função de output

---
+++ module/output.js
@@ -0,0 +1,22 @@
+/******************************************************
+ * Objetivo: Arquivo responsável por funções de saída.
+ * Autor: Leonardo Scotti
+ * Data: 05/08/2025
+ * Versão: 1.0
+ ******************************************************/
+
+function dataOutput(clientName, productName, startingCapital, timesAYear, finalAmount) {
+    let name = clientName
+    let product = productName
+    let capital = Number(startingCapital)
+    let times = Number(timesAYear)
+    let amount = Number(finalAmount)
+
+    let output = console.log('******************* VIVA MODA *******************\n',
+        `Muito obrigado por realizar sua compra conosco Sr(a) ${name}.\n`,
+        `A compra do produto ${product}, tem um valor de: R$${capital}.\n`,
+        `A sua compra será parcelada em ${times} vezes e o Sr(a) pagará: R$${amount}`
+    )
+
+    return output
+}
