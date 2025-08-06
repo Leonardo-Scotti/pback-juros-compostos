633be1a28dccfdff9a3ef3b98a990ef3311e4949
Leonardo-Scotti <scottileonardo1@gmail.com>
2025-08-05 21:37:37 -0300

    Criando entrada de Dados com a biblioteca 'readline'

---
+++ app.js
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Objetivo: Criar um sistema que faz o calculo de juros compostos de um produto
+ * Data: 05/08/2025
+ * Autor: Leonardo Scotti
+ * Versão: 1.0
+ ******************************************************************************/
+
+const readline = require('readline')
+
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
