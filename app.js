a4aa65e409bf1ec5da75966d71ae5295ecac73c1
Leonardo-Scotti <scottileonardo1@gmail.com>
2025-08-05 22:23:53 -0300

    Feita a estrutura de entrada de dados e seus respectivos tratamentos.

--- app.js
+++ app.js
@@ -4,10 +4,59 @@
  * Autor: Leonardo Scotti
  * Versão: 1.0
  ******************************************************************************/
+const INTEREST = require('./module/interest.js')
 
 const readline = require('readline')
 
-const entradaDeDados = readline.createInterface({
+const dataEntry = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+})
+
+dataEntry.question('Digite o nome do cliente: ', function(name) {
+    let clientName = name
+
+    if (!/^[a-zA-Z\u00C0-\u017F\s'-]+$/.test(clientName)) {
+        console.log('ERRO: O nome do cliente deve ser composto apenas por caracteres alfabéticos! Reinicie o programa.')
+    } else {
+        dataEntry.question('Digite o nome do produto: ', function(value) {
+            let productName = value
+
+            dataEntry.question('Digite o Capital Inicial: R$', function(capital) {
+                let startingCapital = Number(capital)
+
+                if (isNaN(startingCapital)) {
+                    console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                } else {
+                    dataEntry.question('Digite a Taxa de Juros Anual: ', function(interestValue) {
+                        let interest = Number(interestValue)
+
+                        if (isNaN(interest)) {
+                            console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                        } else {
+                            let interestRate = INTEREST.calculateInterestPercentage(interest)
+
+                            dataEntry.question('Digite quantas vezes ele será composto por ano: ', function(times) {
+                                let timesAYear = Number(times)
+
+                                if (isNaN(timesAYear)) {
+                                    console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                                } else {
+                                    dataEntry.question('E em quantos anos? ', function(year) {
+                                        let time = year
+
+                                        if (isNaN(time)) {
+                                            console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                                        } else {
+
+                                        }
+                                    })
+                                }
+                            })
+                        }
+                    })
+                }
+            })
+        })
+    }
 })
--- module/interest.js
+++ module/interest.js
@@ -1,17 +1,17 @@
 /******************************************************
- * Objetivo: Arquivo responsável por calcular o juros
+ * Objetivo: Arquivo responsável por calcular o juros.
  * Autor: Leonardo Scotti
  * Data: 05/08/2025
  * Versão: 1.0
  ******************************************************/
 
 //Função que calcula o Montante Final
-function calculateFinalAmount(startingCapital, annualInterestRate, interestPerYear, time) {
+function calculateFinalAmount(startingCapital, annualInterestRate, timesAYear, time) {
 
     let amaount
     let p = Number(startingCapital)
     let r = Number(annualInterestRate)
-    let n = Number(interestPerYear)
+    let n = Number(timesAYear)
     let t = Number(time)
 
     amaount = p * Math.pow((1 + (r / n)), (n * t))
