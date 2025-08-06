364e1be851a6ab7272982155c0d669a808251de6
Leonardo-Scotti <scottileonardo1@gmail.com>
2025-08-05 22:55:09 -0300

    Arrumando tratavida de dados do nome do produto

--- app.js
+++ app.js
@@ -23,45 +23,49 @@ dataEntry.question('Digite o nome do cliente: ', function(name) {
         dataEntry.question('Digite o nome do produto: ', function(value) {
             let productName = value.toUpperCase()
 
-            dataEntry.question('Digite o Capital Inicial: R$', function(capital) {
-                let startingCapital = Number(capital)
+            if (!/^[a-zA-Z\u00C0-\u017F\s'-]+$/.test(clientName)) {
+                console.log('ERRO: O nome do cliente deve ser composto apenas por caracteres alfabéticos! Reinicie o programa.')
+            } else {
+                dataEntry.question('Digite o Capital Inicial: R$', function(capital) {
+                    let startingCapital = Number(capital)
 
-                if (isNaN(startingCapital)) {
-                    console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
-                } else {
-                    dataEntry.question('Digite a Taxa de Juros Anual: ', function(interestValue) {
-                        let interest = Number(interestValue)
+                    if (isNaN(startingCapital)) {
+                        console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                    } else {
+                        dataEntry.question('Digite a Taxa de Juros Anual: ', function(interestValue) {
+                            let interest = Number(interestValue)
 
-                        if (isNaN(interest)) {
-                            console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
-                        } else {
-                            let interestRate = INTEREST.calculateInterestPercentage(interest)
+                            if (isNaN(interest)) {
+                                console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                            } else {
+                                let interestRate = INTEREST.calculateInterestPercentage(interest)
 
-                            dataEntry.question('Digite quantas vezes ele será composto por ano: ', function(times) {
-                                let timesAYear = Number(times)
+                                dataEntry.question('Digite quantas vezes ele será composto por ano: ', function(times) {
+                                    let timesAYear = Number(times)
 
-                                if (isNaN(timesAYear)) {
-                                    console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
-                                } else {
-                                    dataEntry.question('E em quantos anos? ', function(year) {
-                                        let time = year
+                                    if (isNaN(timesAYear)) {
+                                        console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                                    } else {
+                                        dataEntry.question('E em quantos anos? ', function(year) {
+                                            let time = year
 
-                                        if (isNaN(time)) {
-                                            console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
-                                        } else {
-                                            let finalAmount = INTEREST.calculateFinalAmount(startingCapital, interestRate, timesAYear, time)
+                                            if (isNaN(time)) {
+                                                console.log('ERRO: Você deve digitar apenas caracteres numéricos! Reinicie o programa.')
+                                            } else {
+                                                let finalAmount = INTEREST.calculateFinalAmount(startingCapital, interestRate, timesAYear, time)
 
-                                            let output = OUTPUT.dataOutput(clientName, productName, startingCapital, timesAYear, finalAmount)
+                                                let output = OUTPUT.dataOutput(clientName, productName, startingCapital, timesAYear, finalAmount)
 
-                                            return output
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    })
-                }
-            })
+                                                return output
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
         })
     }
 })
